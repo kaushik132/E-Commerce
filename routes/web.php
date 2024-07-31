289a2490e742be21d956b132e4a022bf00aaf6df
kaushik132 <kaushik.dey.ahit@gmail.com>
2024-07-31 17:41:03 +0530

    new page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\admin\AdminLoginController;
 use App\Http\Controllers\admin\HomeController;
+use App\Http\Controllers\admin\CategoryController;
 use Illuminate\Support\Facades\Route;
 use Symfony\Component\CssSelector\XPath\Extension\FunctionExtension;
 
@@ -29,11 +30,16 @@ Route::group(['prefix' => 'admin'],function(){
     Route::group(['middleware' => 'admin.guest'],function(){
         Route::get('/login', [AdminLoginController::class,'index'])->name('admin.login');
         Route::post('/authenticate', [AdminLoginController::class,'authenticate'])->name('admin.authenticate');
+        
     });
 
     Route::group(['middleware' => 'admin.auth'],function(){
         Route::get('/dashboard', [HomeController::class,'index'])->name('admin.dashboard');
         Route::get('/logout', [HomeController::class,'logout'])->name('admin.logout');
+
+        // Category Route
+        Route::get('/categories/create', [CategoryController::class,'create'])->name('categories.create');
+
     });
 
 });
